564e8efb4eb1dd1701cd5edd3a7d986ae3aa2a92
Kyle Lee <hello.kylelee@gmail.com>
2017-03-08 03:30:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function Point(x, y) {
   this.x = x;
   this.y = y;
-  toString = function() {
-    return "(" + this.x + ", " + this.y + ")";
+}
+Point.prototype.toString = function() {
+  return "(" + this.x + ", " + this.y + ")";
+}
+
+function Shape(x, y) {
+  this.addToPlane = function(x, y) {
+    this.position = Point.call(this, x, y);
   }
+}
+
+Shape.prototype.move = function(x, y) {
+  this.position = Point.call(this, x, y);
 }
