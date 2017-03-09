28e0ca73fd954efb75adda191a0c77f8b6db9e6f
Kyle Lee <hello.kylelee@gmail.com>
2017-03-09 03:55:22 +0000

    Done.

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ Point.prototype.toString = function() {
   return "(" + this.x + ", " + this.y + ")";
 }
 
+//creating sides constructor to be used in polygon
+function Side(length) {
+  this.length = length;
+}
+
 //creating overarching shape prototype with function for adding to plane and moving the shape
-function Shape(x, y) {};
+function Shape() {};
 
 Shape.prototype.addToPlane = function(x, y) {
-  return this.position = new Point(x, y);
+  this.position = new Point(x, y);
 }
 Shape.prototype.move = function(x, y) {
-  return this.position = new Point (x, y);
+  this.position = new Point (x, y);
 }
 
 //creating a circle prototype based on shape prototype and extending it
@@ -27,18 +32,13 @@ Circle.prototype = Object.create(Shape.prototype);
 Circle.prototype.constructor = Circle;
 
 Circle.prototype.diameter = function() {
-  return this.radius * 2;
+  return (this.radius * 2);
 }
 Circle.prototype.area = function() {
-  return this.radius * 2;
+  return (Math.PI * this.radius^2);
 }
 Circle.prototype.circumference = function() {
-  return 2 * Math.PI * this.radius;
-}
-
-//creating sides constructor to be used in polygon
-function Side(length) {
-  this.length = length;
+  return (2 * Math.PI * this.radius);
 }
 
 //creating polygon prototype based on shape prototype and extending it
@@ -61,17 +61,51 @@ Polygon.prototype.numberOfSides = function() {
   return this.sides.length;
 }
 
+//creating triangle prototype based on polygon prototype
+function Triangle(sideOneLength, sideTwoLength, sideThreeLength) {
+  Polygon.call(this, [new Side(sideOneLength), new Side(sideTwoLength), new Side(sideThreeLength)]);
+}
+
+Triangle.prototype = Object.create(Polygon.prototype);
+Triangle.prototype.constructor = Triangle;
+
 //creating quadrilateral prototype based on polygon prototype
-//SO THE PROBLEM IS: it is expecting "sides" as an argument, to satisfy Polygon methods, however, there tests don't take
-//into account the "sides" argument when they pass in arguments. Look at the solution to see if it's something you are doing.
-//Pretty sure it's not.
-function Quadrilateral(sides, sideOneLength, sideTwoLength, sideThreeLength, sideFourLength) {
-  Polygon.call(this, sides);
-  this.sideOneLength = sideOneLength;
-  this.sideTwoLength = sideTwoLength;
-  this.sideThreeLength = sideThreeLength;
-  this.sideFourLength = sideFourLength;
+function Quadrilateral(sideOneLength, sideTwoLength, sideThreeLength, sideFourLength) {
+  Polygon.call(this, [new Side(sideOneLength), new Side(sideTwoLength), new Side(sideThreeLength), new Side(sideFourLength)]);
 }
 
 Quadrilateral.prototype = Object.create(Polygon.prototype);
 Quadrilateral.prototype.constructor = Quadrilateral;
+
+//creating rectangle prototype based on quadrilateral prototype
+function Rectangle(width, height) {
+  Quadrilateral.call(this, width, height, width, height);
+  this.width = width;
+  this.height = height;
+}
+
+Rectangle.prototype = Object.create(Quadrilateral.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+Rectangle.prototype.area = function() {
+  return this.width * this.height;
+}
+
+//creating square prototype based on rectangle prototype
+function Square(length) {
+  Rectangle.call(this, length, length);
+  this.length = length;
+}
+
+Square.prototype = Object.create(Rectangle.prototype);
+Square.prototype.constructor = Square;
+
+Square.prototype.listProperties = function() {
+  var arrayOfProperties = [];
+  for (var prop in this) {
+    if(this.hasOwnProperty(prop)) {
+      arrayOfProperties.push("Square." + prop + " = " + this[prop]);
+    }
+  }
+  return arrayOfProperties.join(', ');
+}
